23250af668008273b3ae32efdfdd3213f9767c4e
Hung Duong - Tw <hung.duong@thoughtworks.com>
2023-09-05 10:54:38 +0700

    Add form validation

--- newsletter-sign-up-with-success-message-main/js/__test__/index.test.ts
+++ newsletter-sign-up-with-success-message-main/js/__test__/index.test.ts
@@ -1,26 +1,34 @@
 import { beforeEach, describe, expect, it } from '@jest/globals';
 
 describe("submit form", () => {
-  let documentBody: HTMLBodyElement
   const HtmlFormId = "sign-up--form"
 
   beforeEach(() => {
-    const HtmlForm = `<form class = "sign-up--form" id ="` + HtmlFormId + `">
+    const htmlForm = `<form class = "sign-up--form" id ="` + HtmlFormId + `">
         <label for="sign-up--form--email-address" class="sign-up--form--label">Email address</label>
         <input id="sign-up--form--email-address" class="sign-up--form--input-field curvy mb-1" type="email"
-          placeholder="email@company.com"></input>
+          placeholder="email@company.com">email@company.com</input>
         <button class="sign-up--form--submit-button curvy">Subscribe to monthly newsletter</button>
       </form>`
-    documentBody = document.createElement("body")
-    documentBody.innerHTML = HtmlForm
+    document.body.innerHTML = htmlForm
   });
 
   it("Should run in jsdom environment", () => {
-    expect(documentBody.children.length).toBe(1)
+    expect(document.body.children.length).toBe(1)
   });
 
-  it("Should prevent default form submit for submit click", () => {
-    const htmlForm = documentBody.querySelector("#" + HtmlFormId)
+  it("Should submit form on button click", async () => {
+    const htmlForm = document.getElementById(HtmlFormId)
     expect(htmlForm).toBeInstanceOf(HTMLFormElement)
+
+    const submitButton = htmlForm.getElementsByTagName("button")[0]
+    expect(submitButton).toBeTruthy()
+
+    let eventIsActivated: boolean = false
+    htmlForm.addEventListener("submit", (event) => {
+      eventIsActivated = true
+    })
+    submitButton.click()
+    expect(eventIsActivated).toBeTruthy()
   })
 })
