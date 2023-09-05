1c1ec957c5b29caaca6f338a72bdf6eeb645f5e0
Hung Duong - Tw <hung.duong@thoughtworks.com>
2023-09-05 22:30:03 +0700

    Add Husky Precommit hook

--- newsletter-sign-up-with-success-message-main/scripts/run-e2e-tests.sh
+++ newsletter-sign-up-with-success-message-main/scripts/run-e2e-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -euo pipefail
+BASEDIR=$(dirname $0)
 
 printf "\360\237\215\272\t Running e2e tests...  \n"
+pushd ${BASEDIR}/../
 docker-compose -f "docker-compose-e2e.yml" up --build --abort-on-container-exit && docker-compose -f "docker-compose-e2e.yml" down
